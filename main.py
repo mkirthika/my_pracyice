cf12599344e5e5cc9603b5bd4025919420e6a40b
Kirthika <kirthi125@yahoo.com>
2021-08-01 18:01:00 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#Datatypes
+a=10
+print(type(a)) #should print int(integer)
+b='kirthika'
+print(type(b))  #should print str(string)
+c=10.5
+print(type(c)) #should print float(float)
+d=2+3j
+print(type(d)) #should print complex(complex)
+#   datatypes
+# ----->1. int
+#------->2.string
+#------->3.boolean
+
+
